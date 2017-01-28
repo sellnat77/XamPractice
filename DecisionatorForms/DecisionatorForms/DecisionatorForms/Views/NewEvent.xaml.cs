3e5061398b33a656a807b5c4ad2ab04ef712e698
Russell Tan <sell_nat@yahoo.com>
2017-01-28 13:22:48 -0800

    Added base project for xaml forms, compiled and executed

---
+++ DecisionatorForms/DecisionatorForms/DecisionatorForms/Views/NewEvent.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DecisionatorForms.Views
+{
+    public partial class NewEvent : ContentView
+    {
+        public NewEvent()
+        {
+            InitializeComponent();
+        }
+    }
+}
